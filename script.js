8afc4389e2b872b9865dda43a2d087ca1fc2e72e
CarlosGherbaz <carlosgherbaz@gmail.com>
2024-05-05 16:50:34 -0400

    created the base functions to add the books to the array

--- script.js
+++ script.js
@@ -5,9 +5,28 @@ const cancelBookButton = document.querySelector(".cancel-button");
 const xButton = document.querySelector(".x-button");
 const removeBookWindow = document.querySelector(".remove-book");
 const noButton = document.querySelector(".remove-button-no");
+const newBookButton = document.querySelector(".new-book");
+const formElement = document.querySelector(".form");
+
+//input document selectors
+const userInputName = document.querySelector("#bookName");
+const userInputAuthor = document.querySelector("#bookAuthor");
+const userInputPages = document.querySelector("#bookPages");
 
 //project global variables
 const bookList = [];
+
+//event listeners for buttons
+newBookButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  addBookToLibrary(
+    userInputName.value,
+    userInputAuthor.value,
+    userInputPages.value
+  );
+  formElement.reset();
+  popupWindow.style.display = "none";
+});
 
 //event listeners for the popup windows
 addBookButton.addEventListener("click", () => {
@@ -25,3 +44,21 @@ xButton.addEventListener("click", () => {
 noButton.addEventListener("click", () => {
   removeBookWindow.style.display = "none";
 });
+
+//new book constructor
+function book(name, author, pages) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.completed = false;
+  this.info = () => {
+    return `${this.name} by ${this.author}, has ${this.pages} pages`;
+  };
+}
+
+//project functions
+function addBookToLibrary(nameInput, authorInput, pagesInput) {
+  const newBook = new book(nameInput, authorInput, pagesInput);
+  bookList.push(newBook);
+  console.log(newBook.info());
+}
